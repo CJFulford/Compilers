49884d748618e118ce4d09cc882a97f70e4d4a97
CJFulford <cjfulford@shaw.ca>
2017-01-26 12:24:20 -0700

    added reference to eng_lang.has, more code organization

--- src/main.hs
+++ src/main.hs
@@ -1,3 +1,11 @@
+{-
+    Author: Cody Fulford
+    Purpose: Assignment 1 submission for CPSC 411 at the University of Calgary, W17
+    Notes:
+        majority of the code has been written by myself.
+        Main, lexer, and lexer' funcations have been based on The same functions in "eng_lang.hs" by Prashant Kumar
+-}
+
 import System.Environment
 import Data.Char
 
@@ -92,10 +100,9 @@ lexer' (x:xs)
     | otherwise     = (ERROR (x++" Invalid TOKEN"))     : lexer' xs
     
     
-    
-    
-    
-    
+-- =================================================
+-- COMMENT HANDLING
+
 
 -- found beginning of line comment, ignore everything until after the next new line
 -- this runs as a pre-processor to the lexer, after 'lines' and before 'words' this ensures that a line comment removes the entire line, and since line comments take presedance against block comments, thi will also remove any block comment open or close after the % on this line                  
@@ -119,6 +126,10 @@ cutBlockComment' count (x:xs) = case x of
                     _    -> cutBlockComment' count xs       -- contents of block, keep looking for end
 
                     
+-- =================================================
+-- ID HANDLING           
+
+
 -- checks to see if a string qualifies as an id
 -- need secondary function since the first character has a reduced range of valid posibilities                   
 isID :: [Char] -> Bool
@@ -133,14 +144,11 @@ isID' (x:xs)
         | not (x `elem` (alphaRange++numRange)) = False -- possible ID has characters outside valid range
         | xs == [] = True                               -- no more chars to check, valid ID
         | otherwise = isID' xs                          -- more chars to checks
-
--- checks to see if a string can be a digit
-isNum :: [Char] -> Bool
-isNum (x:xs)
-        | not (isDigit x) = False   -- character is not a digit, cannot be converted to a number, fail
-        | xs == [] = True           -- all characters checks, can be converted to number
-        | otherwise = isNum xs      -- some chars in string still need to be checked
+        
              
+-- =================================================
+-- SYMBOL HANDLING
+
 
 -- inputs is a string, one of the strings following word function in lexer
 -- output = list of strings, function looks at input string and seperates by symbols "a/b" => ["a", "/", "b"]
@@ -158,8 +166,20 @@ merge x [] = [[x]]
 merge x (y:ys)
             | y `elem` strOperators = [x]:y:ys
             | otherwise = [x:y]++ys
+            
+
+ 
+-- =================================================
+-- NUMBER HANDLING 
 
 
+-- checks to see if a string can be a digit
+isNum :: [Char] -> Bool
+isNum (x:xs)
+        | not (isDigit x) = False   -- character is not a digit, cannot be converted to a number, fail
+        | xs == [] = True           -- all characters checks, can be converted to number
+        | otherwise = isNum xs      -- some chars in string still need to be checked
+        
 -- convert a string to an int
 strToInt :: String -> Int
 strToInt [] = 0             -- nothing to convert, return 0
@@ -169,4 +189,3 @@ strToInt x = strToInt' x 0  -- need helper method to keep track of place in stri
 strToInt' :: String -> Int -> Int
 strToInt' [] _ = 0          -- no more to convert, return 0
 strToInt' x c = ((digitToInt (last x)) * (10^c)) + (strToInt' (init x) (c+1)) -- grab the last character in the string, convert it to a digit. to correctly add it to the result, multiply it by 10^count, which is where it was in the string, relative to the back. add all of these results together and the number is complete
-
